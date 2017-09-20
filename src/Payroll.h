ee270835dc833a2936149124df53d14b3cedf1b5
zhongyi1337 <31940663+zhongyi1337@users.noreply.github.com>
2017-09-20 18:46:22 -0500

    payroll header file test

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,5 +1,19 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
+#pragma once
+#include <string>
+using namespace std;
+double ot(double hours);
+double regularpay(double hours, double pay_rate);
+double otpay(double pay_rate, double ot);
+double grosspay(double regularpay, double otpay);
+double grosspay(double salary);
+double getFIT(double grosspay);
+double getFICASSN(double grosspay);
+double getFICAMED(double grosspay);
+double getNetpay(double grosspay, double fit, double ssn, double med);
+string contoStr(double db);
+void printout(string results[]);
 
 
 
