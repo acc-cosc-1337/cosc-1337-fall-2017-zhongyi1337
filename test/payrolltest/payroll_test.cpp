ddfc90e35665324237142e88ccdbbda92f94962b
zhongyi1337 <31940663+zhongyi1337@users.noreply.github.com>
2017-09-27 18:46:13 -0500

    unit test

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -7,3 +7,29 @@ TEST(payroll_test, ovettime_test){
 TEST(payroll_test, grosspay_test){
     EXPECT_EQ(2000, grosspay(52000));
 }
+
+
+TEST(payroll_test, getFICASSN_test){   
+    EXPECT_EQ(124, getFICASSN(2000));
+}
+
+TEST(payroll_test, getFICASSN_test){       
+    EXPECT_EQ(0, getFICASSN(0));
+}
+
+TEST(payroll_test, grosspay_test){
+    EXPECT_EQ(400, grosspay(400, 0));
+}
+
+TEST(payroll_test, grosspay_test){    
+    EXPECT_EQ(860,    grosspay(800, 60));
+}
+
+
+TEST(payroll_test, otpay_test){
+     EXPECT_EQ(30, otpay(20));
+}
+
+TEST(payroll_test, otpay_test){     
+    EXPECT_EQ(0, otpay(0));
+}
