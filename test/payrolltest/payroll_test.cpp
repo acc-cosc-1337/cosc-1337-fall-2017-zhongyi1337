9c1a912d8bcf9864d91e9ba297862de3cf112200
zhongyi1337 <31940663+zhongyi1337@users.noreply.github.com>
2017-09-27 19:39:40 -0500

    Update payroll_test.cpp

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -8,17 +8,18 @@ TEST(payroll_test, grosspay_test){
     EXPECT_EQ(2000, grosspay(52000));
 }
 
-TEST(payroll_test, getFICASSN_test1){   
+TEST (payroll_test, getFICASSN_TEST1){
     EXPECT_EQ(124, getFICASSN(2000));
 }
 
-TEST(payroll_test, getFICASSN_test2){       
+TEST(payroll_test, getFICASSN_test2){
     EXPECT_EQ(62, getFICASSN(1000));
 }
 
-TEST(payroll_test, grosspay_test1){
-    EXPECT_EQ(410, grosspay(400, 10));
+TEST (payroll_test, grosspay_test1){
+    EXPECT_EQ (410, grosspay(400,10));
 }
+
 /*
 TEST(payroll_test, grosspay_test2){    
     EXPECT_EQ(860, grosspay(800, 60));
