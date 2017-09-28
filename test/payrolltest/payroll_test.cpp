5a6df5203f43063c0edd61633eb794141dd43d7d
zhongyi1337 <31940663+zhongyi1337@users.noreply.github.com>
2017-09-27 19:14:50 -0500

    unit test

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -8,28 +8,27 @@ TEST(payroll_test, grosspay_test){
     EXPECT_EQ(2000, grosspay(52000));
 }
 
-/*
-TEST(payroll_test, getFICASSN_test){   
+TEST(payroll_test, getFICASSN_test1){   
     EXPECT_EQ(124, getFICASSN(2000));
 }
 
-TEST(payroll_test, getFICASSN_test){       
+TEST(payroll_test, getFICASSN_test2){       
     EXPECT_EQ(62, getFICASSN(1000));
 }
 
-TEST(payroll_test, grosspay_test){
+TEST(payroll_test, grosspay_test1){
     EXPECT_EQ(410, grosspay(400, 10));
 }
 
-TEST(payroll_test, grosspay_test){    
+TEST(payroll_test, grosspay_test2){    
     EXPECT_EQ(860, grosspay(800, 60));
 }
 
 
-TEST(payroll_test, otpay_test){
+TEST(payroll_test, otpay_test1){
      EXPECT_EQ(30, otpay(20));
 }
 
-TEST(payroll_test, otpay_test){     
+TEST(payroll_test, otpay_test2){     
     EXPECT_EQ(15, otpay(10));
-}*/
+}
